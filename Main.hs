0347d508948a78a74b20ef54ccdaa229166cf61d
maciejspychala <mac.spychala@gmail.com>
2016-12-27 10:56:00 +0100

    route parameters

--- Main.hs
+++ Main.hs
@@ -1,7 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+import Data.Text.Lazy as T
+import Data.Monoid ((<>))
 
-main = scotty 3000 $ do
-  get "/" $ do
-        html "Hello World!"
+routes :: ScottyM ()
+routes = do
+    get "/hello" 
+        hello
+    get "/hello/:name" $ do
+        name <- param "name"
+        helloName name
 
+
+hello :: ActionM ()
+hello = do
+    text "hello world!"
+
+helloName :: Text -> ActionM ()
+helloName name = do
+    text ("hello " <> name <> " :*")
+
+main = scotty 3000 routes
