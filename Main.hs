e29433aad904abdb8afc530e79986f5778c0ce20
maciejspychala <mac.spychala@gmail.com>
2016-12-28 16:01:36 +0100

    database query

--- Main.hs
+++ Main.hs
@@ -4,24 +4,28 @@
 import Web.Scotty
 import Data.Text.Lazy 
 import Data.Monoid ((<>))
+import Control.Monad.IO.Class
 import SqlTypes
 import Data.Aeson (FromJSON, ToJSON, encode)
 import GHC.Generics
 import Database.SQLite.Simple
 import qualified Data.ByteString.Lazy.Char8 as BS
 
-routes :: [User] -> ScottyM ()
-routes allUsers = do
+routes :: Connection -> ScottyM ()
+routes conn = do
     get "/hello" 
         hello
     get "/hello/:name" $ do
         name <- param "name"
         helloName name
     get "/users" $ do
-        json allUsers
+        users <- liftIO ( query_ conn "select id, first_name, second_name, team from users" :: IO [User] )
+        json users
     get "/users/:id" $ do
         id <- param "id"
-        json ([x | x <- allUsers, userId x == id] !! 0)
+        users <- liftIO ( query_ conn "select id, first_name, second_name, team from users" :: IO [User] )
+        json ([x | x <- users, userId x == id] !! 0)
+
 
 hello :: ActionM ()
 hello = do
@@ -33,5 +37,4 @@ helloName name = do
 
 main = do
     conn <- open "data.db"
-    users <- ( query_ conn "select id, first_name, second_name, team from users" :: IO [User] )
-    scotty 3000 (routes users)
+    scotty 3000 (routes conn)
