662d5b7b39ad3570e7283f71b99ae7f872fdc2a4
mewa <marcin.k.chmiel@gmail.com>
2017-04-08 01:44:06 +0200

    Added JWT test

--- test/AuthTest.hs
+++ test/AuthTest.hs
@@ -1,4 +1,10 @@
 import Auth
+import qualified Data.ByteString.Lazy.Char8 as L
+import Control.Monad
 
 main :: IO ()
-main = print $ decrypt "Testing"
+main = do
+    jwk <- readJWK "key.json"
+    Right token <- signUser jwk "mewa"
+    Right ok <- verifyUser jwk token "mewa" 
+    return ok
